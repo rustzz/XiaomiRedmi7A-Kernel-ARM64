75671ff4ddff081ab6a8fbc9ab9844a52e51a5e0
root <alexfinhart@gmail.com>
2021-02-07 09:04:16 +0000

    Tune up extra_free_kbytes & min_free_kbytes values
    For better OOM resistanc

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -261,9 +261,9 @@ compound_page_dtor * const compound_page_dtors[] = {
  * allocations below this point, only high priority ones. Automatically
  * tuned according to the amount of memory in the system.
  */
-int min_free_kbytes = 1024;
+int min_free_kbytes = 18096;
 int user_min_free_kbytes = -1;
-int watermark_scale_factor;
+int watermark_scale_factor = 100;
 
 /*
  * Extra memory for the system to try freeing. Used to temporarily
