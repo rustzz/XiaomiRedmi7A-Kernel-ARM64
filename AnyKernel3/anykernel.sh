e00eb9b447cc5522c50af6e39ae2f7345b46b783
Akira <76020594+AkiraNoSushi@users.noreply.github.com>
2021-03-30 07:42:51 -0400

    Add AnyKernel3 by osm0sis
    This is my own fork, with proper SELinux contexts for kernel modules.

---
+++ AnyKernel3/anykernel.sh
@@ -0,0 +1,36 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Cherry Kernel - by @AkiraNoSushi & @SimplyJoel
+do.devicecheck=1
+do.modules=1
+do.systemless=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=pine
+device.name2=olive
+device.name3=olivelite
+device.name4=olivewood
+device.name4=pinewood
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+# shell variables
+block=/dev/block/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel install
+split_boot;
+flash_boot;
+flash_dtbo;
+## end install
