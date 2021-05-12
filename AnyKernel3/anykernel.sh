fba252ee39cc48320471c403f120602bee25a533
Akira <76020594+AkiraNoSushi@users.noreply.github.com>
2021-05-12 04:18:59 -0400

    anykernel: don't patch lmkd props
    We're using legacy LMK now.

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -55,17 +55,6 @@ ui_print "Patching init..."
 find /system/etc/init/ -type f | while read file; do 
 sed -Ei 's;[^#](write /proc/sys/(kernel|vm)/(sched|dirty|perf_cpu|page-cluster|stat|swappiness|vfs));#\1;g' $file
 done
-# lmkd props
-ui_print "Patching system's build.prop..."
-patch_prop /system/build.prop "ro.lmk.kill_heaviest_task" "true"
-patch_prop /system/build.prop "ro.config.low_ram" "false"
-patch_prop /system/build.prop "ro.lmk.use_minfree_levels" "true"
-patch_prop /system/build.prop "ro.lmk.medium" "300"
-patch_prop /system/build.prop "ro.lmk.critical_upgrade" "true"
-patch_prop /system/build.prop "ro.lmk.upgrade_pressure" "95"
-patch_prop /system/build.prop "ro.lmk.downgrade_pressure" "60"
-patch_prop /system/build.prop "ro.lmk.log_stats" "true"
-patch_prop /system/build.prop "ro.lmk.use_psi" "true"
 # Replace post_boot with ours.
 ui_print "Pushing init.qcom.post_boot.sh..."
 replace_file "/vendor/bin/init.qcom.post_boot.sh" "0755" "init.qcom.post_boot.sh"
